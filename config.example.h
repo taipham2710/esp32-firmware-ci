d5d000d3910407db5cf1b2dc212faefa7f91ec24
taipham2710 <taipham.dev@gmail.com>
2025-06-29 09:40:30 +0700

    chore(config): add example config file for WiFi, MQTT, OTA setup

---
+++ config.example.h
@@ -0,0 +1,8 @@
+// config.example.h
+#pragma once
+#define WIFI_SSID   "your_wifi"
+#define WIFI_PASS   "your_pass"
+#define MQTT_HOST   "your_mqtt"
+#define MQTT_PORT   1883
+#define OTA_SERVER  "http://ip_address:3000"
+#define DEVICE_ID   "esp32-001"
